978cbe9f1a715fbe25dafbcb7ccf95a41ea7fcd5
Jenny Nguy <j.nguy809@gmail.com>
2019-10-08 19:35:26 +0000

    changed char count color to red when exceeding 0 remaining

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   ``
     // const length = val.value.length;
     $("#tweetbox").keyup(function() {
-      messageLength = document.querySelector('#textbox').value.length;
+      messageLength = document.querySelector("#textbox").value.length;
       document.querySelector('#counter').innerHTML=140-messageLength;
+
+      if (document.querySelector('#counter').innerHTML < 0) {
+        document.getElementById("counter").style.color='red'
+      } else {
+        document.getElementById("counter").style.color="#545149"
+      };
     });
 });
