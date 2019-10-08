396f7a3abd6064384d5a5f9dfc37eddd9e7ad380
Jenny Nguy <j.nguy809@gmail.com>
2019-10-08 21:09:07 +0000

    styled tweet boxes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  ``
-    // const length = val.value.length;
     $("#tweetbox").keyup(function() {
       messageLength = document.querySelector("#textbox").value.length;
       document.querySelector('#counter').innerHTML=140-messageLength;
