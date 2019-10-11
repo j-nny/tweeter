ea9a319878ddf7c1d2c8e33250b5fa14f53e5b76
Jenny Nguy <j.nguy809@gmail.com>
2019-10-11 01:51:09 +0000

    fixed error alert slideUp

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#tweetbox").keyup(function() {
     messageLength = $("#textbox").val().length;
     $("#counter").html(140-messageLength);
-    if ($("#counter").html() > 0 && $("#counter").html() <= 140) {
+    if ($("#counter").html() >= 0 && $("#counter").html() <= 140) {
       $("#alert-0").slideUp('fast');
       $("#alert-over140").slideUp('fast')
     }
